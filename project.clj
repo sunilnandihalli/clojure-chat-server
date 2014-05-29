257fcd1710c256de9bd6946060432081edf973d8
Sunil S Nandihalli <sunil.nandihalli@gmail.com>
2014-05-29 09:41:41 +0530

    before moving to a completely async model

--- project.clj
+++ project.clj
@@ -6,6 +6,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [org.clojure/tools.cli "0.3.1"]
+                 [clj-time "0.7.0"]
+                 [beckon "0.1.1"]
                  [aleph "0.3.2"]]
   :main ^:skip-aot chat-server.core
   :target-path "target/%s"
--- src/chat_server/core.clj
+++ src/chat_server/core.clj
@@ -2,6 +2,7 @@
   (:gen-class)
   (:require [clojure.core.async :as async :refer [<! >! <!! >!! timeout chan alt! alts!! go]]
             [clojure.tools.cli :as cli]
+            [beckon :as b]
             [clj-time.core :as t]
             [clojure.java.io :as io]
             [clojure.string :as s]
@@ -28,51 +29,83 @@
                                                     (cons \  (.array (gio/contiguous (gio/encode (cmd-frame cmd) data))))])
                        trf-output-after-decoding (fn [[cmd-str byte-seq]]
                                                    (let [cmd (keyword (s/lower-case cmd-str))
-                                                         data-str (apply str (map char (drop 1 byte-seq)))
-                                                         ret [cmd (gio/decode (cmd-frame cmd) (gio/to-byte-buffer data-str))]]
-                                                     ret))
+                                                         data-str (apply str (map char (drop 1 byte-seq)))]
+                                                     [cmd (gio/decode (cmd-frame cmd) (gio/to-byte-buffer data-str))]))
                        proto-frame [(g/string :ascii :delimiters [" "]) (g/repeated :byte :prefix int-ascii)]]
                    (g/compile-frame proto-frame trf-input-before-encoding trf-output-after-decoding)))
-(let [clients (agent {})]
-  (defn server-handler [ch client-info]
-    (l/receive-all ch (let [my-name (atom nil)]
-                        (fn [[cmd {:keys [to message name]} :as data]]
-                          (case cmd
-                            :reg (if-not @my-name
-                                   (do
-                                     (println name " registered")
-                                     (reset! my-name name)
-                                     (send clients (fn [clients-map]
-                                                     (let [ret (assoc clients-map name ch)]
-                                                       (println (count clients-map) "clients-map update for user " name) ret))))
-                                   (println @my-name " this connection has already registered. request to reg as " name " ignored"))
-                            :snd (if-not @my-name (println (l/enqueue ch [:rcv {:from "chat-server" :word-count 4
-                                                                                :message "please register before messaging"}]))
-                                         (if-let [to-chan (@clients to)]
-                                           (let [resp [:rcv {:from @my-name :word-count (count (s/split message #" +")) :message message}]]
-                                             (l/enqueue to-chan resp))
-                                           (println @my-name " user : " to " has not registered yet " data " request not successful")))))))))
+(defn start-chat-server [server-host server-port]
+  (let [clients (agent {})
+        message-chan
+        error-chan
+        broadcast #(doseq [[name ch] @clients] (l/enqueue ch %))
+        server-handler (fn [ch client-info]
+                         (l/receive-all ch
+                                        (let [my-name (atom nil)]
+                                          (fn [[cmd {:keys [to message name]} :as data]]
+                                            (case cmd
+                                              :reg (if-not @my-name
+                                                     (do
+                                                       (l/enqueue message-chan [:registered name])
+                                                       (reset! my-name name)
+                                                       (send clients (fn [clients-map]
+                                                                       (l/enqueue message-chan [:clients-map-updated
+                                                                                                :clients-count (count clients-map) :name name])
+                                                                       (assoc clients-map name ch))))
+                                                     (l/enqueue error-chan [:my-name @my-name :reg-request-ignored name]))
+                                              :snd (if-not @my-name (l/enqueue ch [:rcv {:from "chat-server" :word-count 4
+                                                                                         :message "please register before messaging"}])
+                                                           (if-let [to-chan (or (@clients to) (if (= my-name to) ch))]
+                                                             (l/enqueue to-chan [:rcv {:from @my-name :message message
+                                                                                       :word-count (count (keep seq (s/split message #" +")))}])
+                                                             (l/enqueue error-chan [:from @my-name :user-not-registered to
+                                                                                    :message-not-sent message]))))))))]
+    (println (str "starting server on port " server-port))
+    (tcp/start-tcp-server server-handler {:host server-host :port server-port :frame frame-codec :name "Sunil's Clojure chat-server"})
+    (reset! (b/signal-atom "INT") #{#(do (println :handling-sigint)
+                                         (broadcast [:bye " "])
+                                         (b/raise! "TERM"))})))
 
 (let [mname #(format "%014d" %)
-      permitted-alphabets "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%&*"
+      permitted-alphabets "abcdefghijklmnopqrstuvwxyz"
       rand-word #(apply str (repeatedly (rand-int 15) (fn [] (rand-nth permitted-alphabets))))
       random-alpha-numeric-string #(s/join " " (repeatedly (rand-int 10) rand-word))]
-  (defn clients-simulation [num-clients server-host server-port]
-    (let [ch (fn [cid] (tcp/tcp-client {:host server-host :port server-port
-                                       :frame frame-codec :name (str (mname cid) "'s client")}))]
-      (l/enqueue ch [:reg {:name name}])
-      (l/wait-for-message ch))))
+  (defn clients-simulation [server-host server-port num-clients num-test-messages]
+    (println (str "starting client spawner pounding chat-server on " server-port))
+    (let [print-chan (logging-channel "log/client.message.log")
+          clients (vec (pmap #(tcp/tcp-client {:host server-host :port server-port
+                                               :frame frame-codec :name (str (mname %) "'s client")})
+                             (range num-clients)))
+          message (fn [id1 id2 msg]
+                    (let [ch1 (l/wait-for-result (clients id1))]
+                      (l/enqueue ch1 [:snd {:to (mname id2) :message msg}])))]
+      (println "parallel setup of tcp-connections complete !")
+      (doseq [c (range num-clients)]
+        (let [client (l/wait-for-result (clients c))
+              my-name (mname c)]
+          (l/receive-all client (fn [[cmd data :as whole]]
+                                  (l/enqueue print-chan (str my-name " : " whole))
+                                  (if (= cmd :bye) (l/close client))))
+          (l/enqueue client [:reg {:name (mname c)}])))
+      (println "sleeping for 3 seconds waiting for all the registrations to go through ")
+      (Thread/sleep 3000)
+      (println "registered " num-clients " users with the chat-server")
+      (doseq [test-msg-id (range num-test-messages)]
+        (message (rand-int num-clients) (rand-int num-clients) (random-alpha-numeric-string)))
+      (println "sent " num-test-messages " random test messages")
+
+      #_(close-logging-channels)
+      (println "closing all logging channels"))))
 
 (let [cli-options [["-p" "--port PORT" "Port number" :default 5000 :parse-fn #(Integer/parseInt %)
                     :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
                    ["-H" "--host hostname" "host name on which the chat-server is running" :default "localhost"]
                    ["-n" "--num-clients number-of-clients" "number of clients" :default 2 :parse-fn #(Integer/parseInt %)]
+                   ["-m" "--num-test-messages number-of-test-messages" :default 10 :parse-fn #(Integer/parseInt %)]
                    ["-s" "--server"] ["-c" "--client"] ["-h" "--help"]]]
   (defn -main [& args]
-    (let [{{:keys [host port server client num-clients] :as options} :options} (cli/parse-opts args cli-options)]
+    (let [{{:keys [host port server client num-clients num-test-messages] :as options} :options help :summary} (cli/parse-opts args cli-options)]
       (cond
-       server (do (println (str "starting server on port " port))
-                  (println (tcp/start-tcp-server server-handler {:host host :port port :frame frame-codec :name "Sunil's Clojure chat-server"}))
-                  (println "tcp/start-tcp-server returned!!"))
-       client (do (println (str "starting client spawner pounding chat-server on " port))
-                  (clients-simulation num-clients host port))))))
+       server (start-chat-server host port)
+       client (clients-simulation host port num-clients num-test-messages)
+       :default (do (println help)
+                    (println "Please indicate the mode in which you want to run this application"))))))
